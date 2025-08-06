1dcbb4b376173a70ed1fd313db9a02d5be5bb3d2
MaxiNiz <maxiniz92@gmail.com>
2025-08-06 15:13:44 -0300

    Añado el archivo del proyecto

---
+++ calculador nutricional.py
@@ -0,0 +1,201 @@
+import sqlite3
+base_datos = 'calculador_nutricional.db'
+IMC_inf = 18.5
+IMC_sup = 24.9
+i = True
+
+
+
+class paciente():
+    def __init__(self, apellido, nombre, edad, peso, talla):
+        
+        self.apellido = apellido
+        self.nombre = nombre
+        self.edad = edad
+        self.peso = peso
+        self.talla = talla
+        self.diagnostico = None
+    def estado_nutricional(self):
+        IMC = self.peso / (self.talla ** 2)   
+        if IMC < IMC_inf:
+            self.diagnostico = 'Bajo Peso'
+            print(f'El IMC del paciente {self.apellido} {self.nombre} es de {IMC}, por lo que se encuentra en {self.diagnostico}\n')
+            
+        elif IMC > IMC_inf and IMC <= IMC_sup:
+            self.diagnostico = 'Normal'
+            print(f"El IMC del paciente {self.apellido} es de {IMC}, por lo que se encuentra dentro de los paramentros normales")
+            
+        elif IMC > IMC_sup:
+            self.diagnostico = 'Sobrepeso'
+            print(f"El IMC del paciente {self.apellido} {self.nombre} es de {IMC}, por lo que se encuentra con {self.diagnostico}")
+            
+        else:
+            pass
+    def dato_comprimido(self):
+        pacie = (1, self.apellido, self.nombre, self.edad, self.peso, self.talla)
+        return pacie
+def conectar_bd():
+    conexion = sqlite3.connect(base_datos)
+    cursor = conexion.cursor()
+    conexion.commit()
+    conexion.close()
+    print(f'Conexion con base de datos {base_datos} exitosa\n')
+def crear_tabla():
+    conexion = sqlite3.connect(base_datos)
+    cursor = conexion.cursor()
+    cursor.execute("""
+           CREATE TABLE IF NOT EXISTS pacientes(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   apellido TEXT NOT NULL,
+                   nombre TEXT NOT NULL,
+                   edad INTEGER NOT NULL,
+                 peso REAL NOT NULL,
+                 talla REAL NOT NULL,
+                   diagnostico TEXT )
+                   
+""")
+    print(f'Tabla creada con exito\n')
+    conexion.commit()
+    conexion.close()
+
+def cargar_datos(persona):
+        conexion = sqlite3.connect(base_datos)
+        cursor = conexion.cursor()
+       
+        instruccion = f"INSERT INTO pacientes(apellido, nombre, edad, peso, talla, diagnostico) VALUES('{persona.apellido}', '{persona.nombre}', '{persona.edad}', '{persona.peso}', '{persona.talla}', '{persona.diagnostico}')"
+        cursor.execute(instruccion)
+        print(f'Paciente {persona.nombre} {persona.apellido} cargado con exito\n')
+        conexion.commit()
+        conexion.close()
+def cargar_varios(persona):
+    conexion = sqlite3.connect(base_datos)
+    cursor = conexion.cursor()
+    instruccion  = f"INSERT INTO pacientes(apellido, nombre, edad, peso, talla, diagnostico) VALUES(?, ?, ?, ?, ?, ?)"
+    cursor.executemany(instruccion, persona)
+    print('pacientes cargados con exito\n')
+    conexion.commit()
+    conexion.close()
+def imprimir_resultados():
+    conexion = sqlite3.connect(base_datos)
+    cursor = conexion.cursor()
+    instruccion = f"SELECT * FROM pacientes"
+    cursor.execute(instruccion)
+    datos = cursor.fetchall()
+    print(datos)
+    conexion.commit()
+    conexion.close()
+def imprimir_orden(columna):
+    conexion = sqlite3.connect(base_datos)
+    cursor = conexion.cursor()
+    instruccion = f"SELECT * FROM pacientes ORDER BY {columna}"
+    cursor.execute(instruccion)
+    datos = cursor.fetchall()
+    print(datos)
+    conexion.commit()
+    conexion.close()
+def buscar():
+    conexion = sqlite3.connect(base_datos)
+    cursor = conexion.cursor()
+    instruccion = f"SELECT * FROM pacientes WHERE nombre like 'JU%'"
+    cursor.execute(instruccion)
+    datos = cursor.fetchall()
+    print(datos)
+    conexion.commit()
+    conexion.close()
+def actualizar():
+    conexion = sqlite3.connect(base_datos)
+    cursor = conexion.cursor()
+    instruccion = f"UPDATE pacientes SET diagnostico='NULL' WHERE nombre like 'JU%'"
+    cursor.execute(instruccion)
+    ins2 = f"SELECT * FROM pacientes"
+    cursor.execute(ins2)
+    datos = cursor.fetchall()
+    print(datos)
+    conexion.commit()
+    conexion.close()
+def borrar():
+    conexion = sqlite3.connect(base_datos)
+    cursor = conexion.cursor()
+    instruccion = f"DELETE FROM pacientes WHERE apellido like 'PER%'"
+    cursor.execute(instruccion)
+    conexion.commit()
+    conexion.close()
+
+def menu_principal():
+        print(f"""
+    ---------------------------------------------
+    Seleccione como desea operar  """)
+        print(f"""
+    ---------------------------------------------
+    | 1) Calcular estado/diagnostico nutricional|
+    | 2) Guardar resultados en base de datos    |
+    | 3) Actualizar un registro                 |
+    | 4) Borrar un registro                     |
+    | 5) Buscar                                 |
+    | 6) Salir                                  |
+          
+""")
+
+class CMR():
+    def __init__(self, archivo_db):
+        #super().__init__(apellido, nombre, edad, peso, talla)
+        self.archivo_db = archivo_db
+        self.conn = None
+        self.cursor = None
+    def informacion(self):
+        print(f"""                        -----------------------------------
+                        BIENVENIDO AL CONTROL NUTRICIONAL
+                        -----------------------------------\n""")
+        last_name = input(f'Ingrese el apellido del paciente\n')
+        name = input(f'ingrese el nombre del paciente\n')
+        years_old = int(input(f'Ingrese la edad del paciente\n'))
+        kg = float(input(f'Ingrese el peso del paciente\n'))
+        altura = float(input(f'Ingrese la talla del paciente (expresada en metros)\n'))
+        paciente.nombre = name
+        paciente.apellido = last_name
+        paciente.edad = years_old
+        paciente.peso = kg
+        paciente.talla = altura
+        
+    def opcion(self):
+        elegido = int(input(''))
+        if elegido == 1:
+            paciente.estado_nutricional(paciente)
+        elif elegido == 2:
+            cargar_datos(paciente)
+        elif elegido == 3:
+            pass
+        elif elegido == 4:
+            pass
+        elif elegido == 5: 
+            pass
+        elif elegido == 6:
+            pass
+        else:
+            pass
+    
+    #def seguir(self):
+        
+
+    
+
+
+
+p1 = paciente('Niz', 'Maximiliano', 33, 70, 1.65)
+p2 = paciente('Perez', 'Juan', 30, 60, 1.65)
+p3 = paciente('Romero', 'Maria', 20, 68, 1.70)
+p4 = CMR(base_datos)
+p4.informacion()
+menu_principal()
+p4.opcion()
+while i == True:
+    sesigue = int(input(f"""
+Quiere realizar otra operacion? 
+1)SI 2)NO\n"""))   
+    if sesigue == 1:
+        menu_principal()
+        p4.opcion()
+    elif sesigue == 2:
+        i = False
+
+
